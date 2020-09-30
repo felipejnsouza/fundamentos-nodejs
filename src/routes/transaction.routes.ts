98b94f28f192219a5bc42d843e00c643f5d5c954
Felipe Souza <fnasci@gmail.com>
2020-09-30 16:43:46 -0300

    create transaction done

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const allTransactions = transactionsRepository.all();
+
+    return response.status(200).json(allTransactions)
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +21,16 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    if(type == 'outcome') {
+      
+      return response.status(400).json({ message: "You don´t have money enough!"})
+    }
+
+    const transaction = transactionsRepository.create({title, type, value})
+
+    return response.status(201).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
